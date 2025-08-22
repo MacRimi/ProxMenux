ae88f7870e18205063eb9ac47625f33ce363b241
MacRimi <ricoextincion@gmail.com>
2025-08-22 18:08:20 +0200

    Update share_menu.sh

--- scripts/menus/share_menu.sh
+++ scripts/menus/share_menu.sh
@@ -28,13 +28,13 @@ while true; do
     OPTION=$(dialog --clear --backtitle "ProxMenux" \
         --title "$(translate "Network Storage Manager")" \
         --menu "\n$(translate "Select an option:")" 25 80 15 \
-        "1" "$(translate "Add NFS Server on LXC")" \
-        "2" "$(translate "Add Samba Server on LXC")" \
-        "3" "$(translate "Config NFS Client on LXC")" \
-        "4" "$(translate "Config Samba Client on LXC")" \
-        "5" "$(translate "Config NFS Host Storage (Proxmox)")" \
-        "6" "$(translate "Config Samba Host Storage (Proxmox)")" \
-        "7" "$(translate "Help & Info commands")" \
+        "1" "$(translate "Set up NFS Server in LXC")" \
+        "2" "$(translate "Set up Samba Server in LXC")" \
+        "3" "$(translate "Configure NFS Client in LXC")" \
+        "4" "$(translate "Configure Samba Client in LXC")" \
+        "5" "$(translate "Configure NFS Storage on Host (Proxmox)")" \
+        "6" "$(translate "Configure Samba Storage on Host (Proxmox)")" \
+        "7" "$(translate "Help & Info (commands)")" \
         "8" "$(translate "Return to Main Menu")" \
         2>&1 >/dev/tty)
     
