10bdecabb68456baa02e4ff5d0d8216742178003
MacRimi <123239993+MacRimi@users.noreply.github.com>
2025-09-03 12:25:35 +0200

    Update share_menu.sh

--- scripts/menus/share_menu.sh
+++ scripts/menus/share_menu.sh
@@ -30,7 +30,7 @@ while true; do
         --menu "\n$(translate "Select an option:")" 25 80 15 \
             ""  "\Z4──────────── $(translate "LXC") ────────────\Zn" \
             "1"         "$(translate "Configure LXC Mount Points    (Host ↔ Container)")" \
-            ""         "" \
+            ""          "" \
             "2"         "$(translate "Configure NFS Client in LXC   (only privileged)")" \
             "3"         "$(translate "Configure Samba Client in LXC (only privileged)")" \
             "4"         "$(translate "Set up NFS Server in LXC      (only privileged)")" \
