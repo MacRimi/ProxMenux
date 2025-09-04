b32241082d64f15cd54e21ce514058745fd31fd9
MacRimi <ricoextincion@gmail.com>
2025-09-04 20:12:24 +0200

    Update share_menu.sh

--- scripts/menus/share_menu.sh
+++ scripts/menus/share_menu.sh
@@ -28,19 +28,18 @@ while true; do
     OPTION=$(dialog --colors --backtitle "ProxMenux" \
         --title "$(translate "Mount and Share Manager")" \
         --menu "\n$(translate "Select an option:")" 25 80 15 \
-            ""  "\Z4──────────── $(translate "LXC") ────────────\Zn" \
-            "1"         "$(translate "Configure LXC Mount Points    (Host ↔ Container)")" \
-            ""          "" \
-            "2"         "$(translate "Configure NFS Client in LXC   (only privileged)")" \
-            "3"         "$(translate "Configure Samba Client in LXC (only privileged)")" \
-            "4"         "$(translate "Set up NFS Server in LXC      (only privileged)")" \
-            "5"         "$(translate "Set up Samba Server in LXC    (only privileged)")" \
             "" "\Z4──────────── $(translate "HOST") ─────────────\Zn" \
-            "6"         "$(translate "Configure NFS shared   on Host")" \
-            "7"         "$(translate "Configure Samba shared on Host")" \
-            "8"         "$(translate "Configure Local Shared on Host")" \
+            "1"         "$(translate "Configure NFS shared   on Host")" \
+            "2"         "$(translate "Configure Samba shared on Host")" \
+            "3"         "$(translate "Configure Local Shared on Host")" \
+            ""  "\Z4──────────── $(translate "LXC") ────────────\Zn" \
+            "4"         "$(translate "Configure LXC Mount Points    (Host ↔ Container)")" \
             ""          "" \
-            "9"         "$(translate "Help & Info (commands)")" \
+            "5"         "$(translate "Configure NFS Client in LXC   (only privileged)")" \
+            "6"         "$(translate "Configure Samba Client in LXC (only privileged)")" \
+            "7"         "$(translate "Set up Samba Server in LXC    (only privileged)")" \
+            ""          "" \
+            "8"         "$(translate "Help & Info (commands)")" \
             "0"         "$(translate "Return to Main Menu")" \
             2>&1 >/dev/tty
     ) || { exec bash <(curl -s "$REPO_URL/scripts/menus/main_menu.sh"); }
@@ -52,30 +51,27 @@ while true; do
             ;;
 
         1)
-            bash <(curl -s "$REPO_URL/scripts/share/lxc-mount-manager.sh")
-            ;;
-        2)
-            bash <(curl -s "$REPO_URL/scripts/share/nfs_client.sh")
-            ;;
-        3)
-            bash <(curl -s "$REPO_URL/scripts/share/samba_client.sh")
-            ;;    
-        4)
-            bash <(curl -s "$REPO_URL/scripts/share/nfs.sh")
-            ;;
-        5)
-            bash <(curl -s "$REPO_URL/scripts/share/samba.sh")
-            ;;    
-        6)
             bash <(curl -s "$REPO_URL/scripts/share/nfs_host_auto.sh")
             ;;
-        7)
+        2)
             bash <(curl -s "$REPO_URL/scripts/share/samba_host.sh")
             ;;
-        8)  
+        3)
             bash <(curl -s "$REPO_URL/scripts/share/local-shared-manager.sh")
+            ;;    
+        4)
+            bash <(curl -s "$REPO_URL/scripts/share/lxc-mount-manager.sh")
             ;;
-        9)
+        5)
+            bash <(curl -s "$REPO_URL/scripts/share/nfs_client.sh")
+            ;;    
+        6) 
+            bash <(curl -s "$REPO_URL/scripts/share/samba_client.sh")
+            ;;
+        7)
+            bash <(curl -s "$REPO_URL/scripts/share/samba.sh")
+            ;;
+        8)
             bash <(curl -s "$REPO_URL/scripts/share/commands_share.sh")
             ;;
         0)
