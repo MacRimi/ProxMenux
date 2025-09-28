3c33d5982caf5fb07c6bd24c2095beb30273d636
MacRimi <ricoextincion@gmail.com>
2025-09-28 23:51:06 +0200

    Update AppImage

--- AppImage/app/page.tsx
+++ AppImage/app/page.tsx
@@ -1,9 +1,7 @@
+"use client"
+
 import { ProxmoxDashboard } from "../components/proxmox-dashboard"
 
 export default function Home() {
-  return (
-    <main className="min-h-screen bg-background">
-      <ProxmoxDashboard />
-    </main>
-  )
+  return <ProxmoxDashboard />
 }
