0ffe1272fe0cdeb2937c695cd4db7aab6a507c58
MacRimi <ricoextincion@gmail.com>
2025-11-18 19:24:23 +0100

    Update virtual-machines.tsx

--- AppImage/components/virtual-machines.tsx
+++ AppImage/components/virtual-machines.tsx
@@ -172,13 +172,6 @@ const fetcher = async (url: string) => {
   return fetchApi(url)
 }
 
-const formatBytes = (bytes: number | undefined): string => {
-  if (!bytes || bytes === 0) return "0 B"
-  const k = 1024
-  const sizes = ["B", "KB", "MB", "GB", "TB"]
-  const i = Math.floor(Math.log(bytes) / Math.log(k))
-  return `${(bytes / Math.pow(k, i)).toFixed(2)} ${sizes[i]}`
-}
 
 const formatUptime = (seconds: number) => {
   const days = Math.floor(seconds / 86400)
